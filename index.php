8426a8ccd35b456e372c41f667a3dac22e6fea06
shiba7208 <144056083+shiba7208@users.noreply.github.com>
2023-09-04 20:18:58 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,57 +1,38 @@
-<?php
-// Определите целевой URL, на который будут перенаправляться разрешенные IP
-$targetUrl = "http://emaillink.rambler-icq.ru/5/lopar.php";
 
-// Получите IP-адрес пользователя
-$userIP = $_SERVER['REMOTE_ADDR'];
 
-// Определите путь к файлу для записи IP-адресов
-$logFilePath = 'access_log.txt';
-
-// Откройте файл для записи
-$logFile = fopen($logFilePath, 'a');
-
-// Чтение списка запрещенных IP-адресов и сетей из файла
-$blockedIPs = file('blocked_ips.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-
-// Проверьте, является ли IP-адрес пользователя заблокированным
-$isBlocked = false;
-foreach ($blockedIPs as $blockedIP) {
-    if (ipMatch($userIP, $blockedIP)) {
-        $isBlocked = true;
-        break;
-    }
-}
-
-// Запишите IP-адрес пользователя в лог-файл
-$logMessage = date('Y-m-d H:i:s') . ' - ' . $userIP . ' - ' . ($isBlocked ? 'Запрещенный' : 'Разрешенный') . "\n";
-fwrite($logFile, $logMessage);
-
-// Закройте лог-файл
-fclose($logFile);
-
-// Если IP-адрес заблокирован, выполните редирект на другой URL
-if ($isBlocked) {
-    header("Location: https://datki.net/pozhelaniya/horoshego-dnya/");
-    exit;
-}
-
-// IP-адрес не заблокирован, выполните редирект на целевой URL
-header("Location: $targetUrl");
-exit;
-
-// Функция для проверки соответствия IP-адреса сети
-function ipMatch($ip, $subnet)
-{
-    if (strpos($subnet, '/') !== false) {
-        // Это сеть IP в формате "IP/префикс"
-        list($subnetIP, $subnetPrefix) = explode('/', $subnet);
-        $subnetIP = rtrim($subnetIP, '.');
-        $subnetMask = ~((1 << (32 - $subnetPrefix)) - 1);
-        return (ip2long($ip) & $subnetMask) == (ip2long($subnetIP) & $subnetMask);
-    } else {
-        // Это одиночный IP-адрес
-        return $ip == $subnet;
-    }
-}
-?>
+<html xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office" lang="en">
+<head>
+<title></title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<!--[if mso]><xml></xml><![endif]--><style>
+*{box-sizing:border-box}body{margin:0;padding:0}a[x-apple-data-detectors]{color:inherit!important;text-decoration:inherit!important}#MessageViewBody a{color:inherit;text-decoration:none}p{line-height:inherit}.desktop_hide,.desktop_hide table{mso-hide:all;display:none;max-height:0;overflow:hidden}.image_block img+div{display:none} @media (max-width:620px){.mobile_hide{display:none}.row-content{width:100%!important}.stack .column{width:100%;display:block}.mobile_hide{min-height:0;max-height:0;max-width:0;overflow:hidden;font-size:0}.desktop_hide,.desktop_hide table{display:table!important;max-height:none!important}}
+</style>
+</head>
+<body style="background-color:#ddecff;margin:0;padding:0;-webkit-text-size-adjust:none;text-size-adjust:none">
+<table class="nl-container" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace:0;mso-table-rspace:0;background-color:#ddecff"><tbody><tr><td><table class="row row-1" align="center" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace:0;mso-table-rspace:0"><tbody><tr><td><table class="row-content stack" align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace:0;mso-table-rspace:0;color:#000;width:600px;margin:0 auto" width="600.00"><tbody><tr><td class="column column-1" width="100%" style="mso-table-lspace:0;mso-table-rspace:0;font-weight:400;text-align:left;padding-bottom:5px;padding-top:5px;vertical-align:top;border-top:0;border-right:0;border-bottom:0;border-left:0">
+<table class="image_block block-1" width="100%" border="0" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace:0;mso-table-rspace:0"><tr><td class="pad" style="width:100%;padding-right:0;padding-left:0"><div class="alignment" align="center" style="line-height:10px"><img src="https://media1.giphy.com/media/xAqHUL21pMHe0/giphy.gif?cid=20eb4e9d7yinvoe4bnrtd5n79yr2ayrfskwbym7hcwhrf6f6&amp;ep=v1_stickers_trending&amp;rid=giphy.gif&amp;ct=s" style="display:block;height:auto;border:0;max-width:120px;width:100%" width="120" alt="Image" title="Image"></div></td></tr></table>
+<table class="text_block block-2" width="100%" border="0" cellpadding="10" cellspacing="0" role="presentation" style="mso-table-lspace:0;mso-table-rspace:0;word-break:break-word"><tr><td class="pad"><div style="font-family:Georgia,'Times New Roman',serif"><div class style="font-size:14px;font-family:Georgia,Times,'Times New Roman',serif;mso-line-height-alt:21px;color:#000;line-height:1.5"><p style="margin:0;font-size:18px;text-align:left;mso-line-height-alt:27px">Крепчайшего здоровья, долгих лет и много радостных минут! Пусть в доме царит порядок и уют, пусть радость и любовь согревают душу, а забота и внимание родных поднимают настроение. Пусть всегда находится самый лучший вариант для решения задач и принимается самое мудрое решение!</p></div></div></td></tr></table>
+</td></tr></tbody></table></td></tr></tbody></table></td></tr></tbody></table>
+<!-- End --><table bgcolor="white" align="left" width="100%"><tr><td><span style="font-family: arial,helvetica,sans-serif; color: black; font-size: 12px;"><table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" height="65" style="width:100%;max-width:650px;height:65px;">
+	<tbody>
+<tr>
+		<td style="font-size:0;vertical-align:middle;" align="center">
+			<table align="center" cellpadding="0" cellspacing="0" border="0" width="212" height="70" style="height:70px;vertical-align:top;max-width:212px;border-spacing:0px;">
+				<tbody>
+<tr>
+					<td style="vertical-align:middle;width:100%;height:100%;" valign="middle" align="center">
+						<a style="text-decoration:none;font-family:Arial;color:#9B9B9B;font-size:13px;" href="https://www.unisender.com/?utm_source=free_tarif_email&amp;utm_medium=email&amp;utm_campaign=futerdarom3" target="_blank">сделано в</a>
+						<a href="https://www.unisender.com/?utm_source=free_tarif_email&amp;utm_medium=email&amp;utm_campaign=futerdarom4" target="_blank">
+							<img src="http://s.usndr.com/marketing/logo-footer3.png" width="180" height="25" border="0" alt="Unisender" title="Unisender">
+						</a>
+					</td>
+				</tr>
+			</tbody>
+</table>
+		</td>
+	</tr>
+</tbody>
+</table>
+<br></span></td></tr></table>
+</body>
+</html>
